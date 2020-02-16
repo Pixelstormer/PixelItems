bd430755451204d1c74fc7bab377fc3299014dd5
Pixelstorm <thepixelstormer@gmail.com>
2020-02-16 14:23:27 +0000

    Update jouster rarity using ItemRarityID over ints

--- Items/Armour/Jouster/JousterBody.cs
+++ Items/Armour/Jouster/JousterBody.cs
@@ -17,7 +17,7 @@ namespace PixelItems.Items.Armour.Jouster
 			item.width = 18;
 			item.height = 18;
 			item.value = Item.sellPrice (0, 0, 82, 0);
-			item.rare = 1;
+			item.rare = ItemRarityID.Green;
 			item.defense = 5;
 		}
 
