73b219c9c742f865dc27184b9f63ce6c4c1a3bac
Pixelstorm <thepixelstormer@gmail.com>
2020-02-17 16:10:25 +0000

    Add PixelPlayer class

---
+++ PixelPlayer.cs
@@ -0,0 +1,33 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using PixelItems.Utils;
+
+namespace PixelItems
+{
+	public class PixelPlayer : ModPlayer
+	{
+		public bool hasMirrorShield = false;
+		public int mirrorShieldCooldownFrames = 0;
+
+		private int lastMirrorShieldProc = 0;
+
+		public override void ResetEffects ()
+		{
+			hasMirrorShield = false;
+			mirrorShieldCooldownFrames = int.MaxValue;
+		}
+
+		public override bool CanBeHitByProjectile (Projectile proj)
+		{
+			if (hasMirrorShield && PixelItems.frameCount - lastMirrorShieldProc > mirrorShieldCooldownFrames)
+			{
+				lastMirrorShieldProc = PixelItems.frameCount;
+				return false;
+			}
+			else
+				return base.CanBeHitByProjectile (proj);
+		}
+	}
+}
